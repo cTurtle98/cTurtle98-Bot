6ed98f3bf4b563b18c166074956773fef7d6e0e1
Ciaran Farley <ciaran@cturtle98.com>
2020-06-11 00:36:05 -0700

    add whitelist method

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,17 @@
 # bot.py
+
+#os access
 import os
 
+# discord bot api
 import discord
+# enviromental variables importer
 from dotenv import load_dotenv
+# make api requests to minecraft-api
+import requests
+
+#thing to identify commands on server
+PREFIX = '!'
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,5 +21,36 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+# register an event handler
+@client.event
+async def on_message(message):
+    
+    # ignore myself
+    if message.author == client.user:
+        return
+
+    # minecraft
+    if message.channel.category == "minecraft":
+	    # minecraft whitelist command
+	    if message.content.startswith(PREFIX + 'whitelist'):
+	    	# add to list
+	    	if message.content.startswith(PREFIX + 'whitelist add'):
+
+	    		#get the list of users to add
+	    		uname = message.content.split('add', 1)[1]
+
+	    		# make the request to minecraft api
+	    		r = requests.post(message.channel + ".root.cturtle98.com:8080/whitelist/add/", u=uname)
+
+	    			if r.ok:
+	    				await message.channel.send(message.author.mention + " those user(s) should now be on the whitelist")
+	    			else:
+	    				await message.channel.send(message.author.mention + " ERROR! Please contact Ciaran!")
+	    	
+	    	else:
+	    		await message.channel.send(message.author.mention + " Usage: !whitelist add <username. OR whitelist remove <username>")
+
+
 
 client.run(TOKEN)
